4145c57adea277942e3c607284b1e5ea78a9a359
MwendaEric <eric.mwenda@icloud.com>
2023-04-19 15:39:21 +0300

    Final Commit

--- src/main/java/com/ericmwenda/contentcalendar/config/HomepageConfiguration.java
+++ src/main/java/com/ericmwenda/contentcalendar/config/HomepageConfiguration.java
@@ -6,8 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 //This class values are defined in the application.properties file with the variable names starting with cc.
 //This configuration class has to be enabled in the root start application class containing the main() method.
 @ConfigurationProperties(value = "cc")
-//Annotation for externalized configuration.  jkkj
+//Annotation for externalized configuration.
 // Add this to a class definition or a @Bean method in a @Configuration class
-// if you want to bind and validate some external Properties (e.g. from a .properties file).
+// if you want to bind and validate some external Properties ( e.g. from a .properties file).
 public record HomepageConfiguration(String welcomeMessage, String about) {
 }
