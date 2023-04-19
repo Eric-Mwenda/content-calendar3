cf036603c40a5fdc2bfa2c42a3fbaac139c86c6c
MwendaEric <eric.mwenda@icloud.com>
2023-04-19 15:43:13 +0300

    Configured Railway Postgres

--- src/main/java/com/ericmwenda/contentcalendar/config/HomepageConfiguration.java
+++ src/main/java/com/ericmwenda/contentcalendar/config/HomepageConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(value = "cc")
 //Annotation for externalized configuration.
 // Add this to a class definition or a @Bean method in a @Configuration class
-// if you want to bind and validate some external Properties ( e.g. from a .properties file).
+// if you want to bind and validate some external Properties (e.g. from a .properties file).
 public record HomepageConfiguration(String welcomeMessage, String about) {
 }
